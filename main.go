a0a9a5aecd2403e789753d6468fea49940205207
Siddhartha Basu <biosidd@gmail.com>
2016-06-21 14:53:55 +0000

    Moved database argument to global section Fixed bunch of syntax errors

--- main.go
+++ main.go
@@ -17,6 +17,31 @@ func main() {
 			Name:  "log,l",
 			Usage: "Name of the log file(optional), default goes to stderr",
 		},
+		cli.StringFlag{
+			Name:   "user, u",
+			Usage:  "chado database user[REQUIRED]",
+			EnvVar: "CHADO_USER",
+		},
+		cli.StringFlag{
+			Name:   "password, p",
+			Usage:  "chado database password[REQUIRED]",
+			EnvVar: "CHADO_PASS",
+		},
+		cli.StringFlag{
+			Name:   "database, db",
+			Usage:  "chado database name[REQUIRED]",
+			EnvVar: "CHADO_DB",
+		},
+		cli.StringFlag{
+			Name:   "host, h",
+			Usage:  "chado database host[REQUIRED]",
+			EnvVar: "CHADO_HOST",
+		},
+		cli.IntFlag{
+			Name:  "port, p",
+			Usage: "server port",
+			Value: 9998,
+		},
 	}
 	app.Commands = []cli.Command{
 		{
@@ -25,35 +50,11 @@ func main() {
 			Action: commands.RunServer,
 			Flags: []cli.Flag{
 				cli.StringFlag{
-					Name:   "user, u",
-					Usage:  "chado database user[REQUIRED]",
-					EnvVar: "CHADO_USER",
-				},
-				cli.StringFlag{
-					Name:   "password, p",
-					Usage:  "chado database password[REQUIRED]",
-					EnvVar: "CHADO_PASS",
-				},
-				cli.StringFlag{
-					Name: "database, db",
-					Usage: "chado database name[REQUIRED]",
-					EnvVar: "CHADO_DB"
-				},
-				cli.StringFlag{
-					Name: "host, h",
-					Usage: "chado database host[REQUIRED]",
-					EnvVar: "CHADO_HOST",
-				}
-				cli.IntFlag{
-					Name:  "port, p",
-					Usage: "server port",
-					Value: 9998,
-				},
-				cli.StringFlag{
-					Name: "sql-file",
+					Name:  "sql-file",
 					Usage: "Text file with sql queries, default is bundled with the package",
-				}
+				},
 			},
 		},
+	}
 	app.Run(os.Args)
 }
